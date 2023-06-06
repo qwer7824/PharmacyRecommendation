95c24aaccfef0a8f558afd2a8b19c3019515ee60
KSH <sh7824@naver.com>
2023-06-06 15:19:02 +0900

    #7 - Kakao Api Test
    레파지토리 테스트에서 테스트도중에 데이터가 중복이 될 가능성이 있어서 `def setup()` 메소드를 이용해 실행할때마다 모든 데이터를 지우는 `deleteAll()` 을 사용하였다.

---
+++ src/test/groovy/com/pharmacyrecommendation/api/service/KakaoAddressSearchServiceTest.groovy
@@ -0,0 +1,38 @@
+package com.pharmacyrecommendation.api.service
+
+import com.pharmacyrecommendation.AbstractIntegrationContainerBaseTest
+import org.springframework.beans.factory.annotation.Autowired
+import spock.lang.Specification
+
+class KakaoAddressSearchServiceTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    private KakaoAddressSearchService kakaoAddressSearchService;
+
+    def "address 파라메터 값이 null이면, requestAddressSearch 메소드는 null을 리턴한다."(){
+        given:
+        String address = null
+
+        when:
+        def result = kakaoAddressSearchService.requestAddressSearch(address)
+
+        then:
+        result == null
+    }
+
+    def "주소값이 valid 하다면 , requestAddressSearch 메소드는 정상적으로 document를 반환한다."(){
+        given:
+
+        def address = "서울 성북구 종암로 10길"
+
+        when:
+        def result = kakaoAddressSearchService.requestAddressSearch(address)
+
+        then:
+        result.documentList.size() > 0
+        result.metaDto.totalCount > 0
+        result.documentList.get(0).addressName != null
+
+    }
+
+}
