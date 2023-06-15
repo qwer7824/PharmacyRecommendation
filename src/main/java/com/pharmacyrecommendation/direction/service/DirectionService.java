d9b1e765146d494d18eaffaee1866740695ec234
KSH <sh7824@naver.com>
2023-06-15 19:25:06 +0900

    #21 - SaveAll Service
    리스트가 비어있는지 검증하고 리스트를 전부다 저장하는 로직

--- src/main/java/com/pharmacyrecommendation/direction/service/DirectionService.java
+++ src/main/java/com/pharmacyrecommendation/direction/service/DirectionService.java
@@ -3,11 +3,14 @@ package com.pharmacyrecommendation.direction.service;
 
 import com.pharmacyrecommendation.api.dto.DocumentDto;
 import com.pharmacyrecommendation.direction.entity.Direction;
+import com.pharmacyrecommendation.direction.repository.DirectionRepository;
 import com.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
 import com.pharmacyrecommendation.pharmacy.service.PharmacySearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -24,6 +27,14 @@ public class DirectionService {
     private static final double RADIUS_KM = 10.0; // 반경 10KM
 
     private final PharmacySearchService pharmacySearchService;
+    private final DirectionRepository directionRepository;
+
+    @Transactional
+    public List<Direction> saveAll(List<Direction> directionList){
+        if(CollectionUtils.isEmpty(directionList))return Collections.emptyList();
+        return directionRepository.saveAll(directionList);
+
+    }
 
 
     public List<Direction> buildDirectionList(DocumentDto documentDto){
