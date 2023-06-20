4f8c458321aae125770b51c6cae65aebd4312aeb
KSH <sh7824@naver.com>
2023-06-20 15:45:49 +0900

    #25 - main page 일부 구현 (검색)
    틀만 잡아놈

---
+++ src/main/java/com/pharmacyrecommendation/direction/controller/FormController.java
@@ -0,0 +1,19 @@
+package com.pharmacyrecommendation.direction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class FormController {
+
+
+    @GetMapping("/")
+    public String main(){
+
+
+        return "main";
+    }
+
+}
