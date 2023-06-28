8db70dcbdccf7cf2d046e82ff9a06e42ada1d043
KSH <sh7824@naver.com>
2023-06-28 15:45:10 +0900

    #27 - base62 리펙토링
    컨트롤러에 있는 비지니스 로직을 서비스로 옮김

--- src/main/java/com/pharmacyrecommendation/direction/service/DirectionService.java
+++ src/main/java/com/pharmacyrecommendation/direction/service/DirectionService.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
 import java.util.Comparator;
@@ -26,6 +27,8 @@ public class DirectionService {
 
     private static final int MAX_SEARCH_COUNT = 3; // 약국 최대 검색 갯수
     private static final double RADIUS_KM = 10.0; // 반경 10KM
+    private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
+
 
     private final PharmacySearchService pharmacySearchService;
     private final DirectionRepository directionRepository;
@@ -39,9 +42,17 @@ public class DirectionService {
 
     }
 
-    public Direction findById(String encodeId){
+    public String findDirectionUrlById(String encodeId){
         Long decodedId = base62Service.decodeDirectionId(encodeId);
-        return directionRepository.findById(decodedId).orElse(null);
+        Direction direction = directionRepository.findById(decodedId).orElse(null);
+
+        String params = String.join(",", direction.getTargetPharmacyName(),
+                String.valueOf(direction.getTargetLatitude()), String.valueOf(direction.getTargetLongitude()));
+
+        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params)
+                .toUriString();
+
+        return result;
     }
 
 
