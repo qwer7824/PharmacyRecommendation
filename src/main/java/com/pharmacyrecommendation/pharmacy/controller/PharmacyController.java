88edaa372b60339957cbc4202181544eec895c7f
KSH <sh7824@naver.com>
2023-07-03 16:17:22 +0900

    #29 - Controller
    RestController 로 임시 save 메소드 작성

---
+++ src/main/java/com/pharmacyrecommendation/pharmacy/controller/PharmacyController.java
@@ -0,0 +1,42 @@
+package com.pharmacyrecommendation.pharmacy.controller;
+
+import com.pharmacyrecommendation.pharmacy.cache.PharmacyRedisTemplateService;
+import com.pharmacyrecommendation.pharmacy.dto.PharmacyDto;
+import com.pharmacyrecommendation.pharmacy.service.PharmacyRepositoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class PharmacyController {
+
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+    private final PharmacyRedisTemplateService pharmacyRedisTemplateService;
+
+    // 데이터 초기 셋팅을 위한 임시 메서드
+
+    @GetMapping("/redis/save")
+    public String save(){
+
+        List<PharmacyDto> pharmacyDtoList = pharmacyRepositoryService.findAll()
+                .stream().map(pharmacy -> PharmacyDto.builder()
+                        .id(pharmacy.getId())
+                        .pharmacyName(pharmacy.getPharmacyName())
+                        .pharmacyAddress(pharmacy.getPharmacyAddress())
+                        .latitude(pharmacy.getLatitude())
+                        .longitude(pharmacy.getLongitude())
+                        .build())
+                .collect(Collectors.toList());
+
+        pharmacyDtoList.forEach(pharmacyRedisTemplateService::save);
+
+        return "success";
+    }
+}
